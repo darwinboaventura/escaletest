256de5e429619604cfed963626123549b3ecd2cb
Darwin Boaventura <darwinboaventura@gmail.com>
2017-03-05 18:10:54 -0300

    adicionado limpeza da ordenacao

--- js/controllers/starsControllers.js
+++ js/controllers/starsControllers.js
@@ -2,4 +2,8 @@ app.controller('starsControllers', function($scope, githubService) {
 	githubService.getAll('wilfernandesjr').then(function(response) {
 		$scope.cards = response;
 	});
+
+	$scope.clearOrderFilter = function() {
+		$scope.orderFilter = '';
+	}
 });
